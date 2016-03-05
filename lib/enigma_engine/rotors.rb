d24b7958a7ff24734ad18cd24255e87897a1769c
andela-sdamian <simon.peter@andela.com>
2016-03-05 23:22:21 +0100

    Refactored rotors.rb

--- lib/enigma_engine/rotors.rb
+++ lib/enigma_engine/rotors.rb
@@ -5,22 +5,22 @@ module EnigmaEngine
     end
 
     def put_key(x, y)
-      key.to_s.split('')[x, y].join('').to_i
+      @key.to_s.split('')[x, y].join('').to_i
     end
 
-    def Rotors.a
+    def a
       put_key(0, 2)
     end
 
-    def Rotors.b
+    def b
       put_key(1, 2)
     end
 
-    def Rotors.c
+    def c
       put_key(2, 2)
     end
 
-    def Rotors.d
+    def d
       put_key(3, 2)
     end
   end
