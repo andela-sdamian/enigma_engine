ee73d329345883edfaf1348a6c487440e9b04a0e
andela-sdamian <simon.peter@andela.com>
2016-03-09 19:17:22 +0100

    Modified Offsest.rb why? This class lacks a method that should return the last four digit of the squared date. if this method wasn't present, it would make it difficult to test the program. So, a new method was included to fix this.

--- lib/enigma_engine/offsets.rb
+++ lib/enigma_engine/offsets.rb
@@ -1,6 +1,6 @@
 module EnigmaEngine
   class Offsets
-    attr_reader :date 
+    attr_reader :date
     def initialize(date)
       @date = date   
     end
@@ -11,25 +11,27 @@ module EnigmaEngine
       date.to_i**2    
     end
     
-    def last_four(index) 
-      @last_four ||= date_squared.to_s.split('')[-4, 4].map(&:to_i)
-      @last_four[index]
+    def last_four_num 
+      date_squared.to_s.split('')[-4, 4].map(&:to_i)
+    end
+    def pick (index) 
+        last_four_num[index]
     end
     
     def a
-      last_four(0)
+      pick(0)
     end
 
     def b
-      last_four(1)
+      pick(1)
     end
 
     def c
-      last_four(2)
+      pick(2)
     end
 
     def d
-      last_four(3)
+      pick(3)
     end
   end
 end
